d341536d9899d38ea8dcbc8a347612f5aee6e84e
Bennet Bradley <bmbmg9@umsystem.edu>
2026-01-23 10:15:02 -0600

    Added the HelloWorld project to the repo

---
+++ HelloWorldApp/HelloWorldApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Bradley, Bennet (MU-Student) on 1/23/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
